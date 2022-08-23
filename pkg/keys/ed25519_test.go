9a08e9d2947eaae9f63d30578fcf81224c83ca0a
neil <42328488+neilnaveen@users.noreply.github.com>
2022-08-22 19:00:34 -0500

    New: Fuzzing for ed25519
    - Included fuzzing test for ed25519.

--- pkg/keys/ed25519_test.go
+++ pkg/keys/ed25519_test.go
@@ -8,6 +8,7 @@ import (
 	"errors"
 	"io"
 	"strings"
+	"testing"
 
 	fuzz "github.com/google/gofuzz"
 	"github.com/theupdateframework/go-tuf/data"
@@ -16,6 +17,7 @@ import (
 
 type Ed25519Suite struct{}
 
+const keySize = 32 // The ed25519 seed size is 32
 var _ = Suite(&Ed25519Suite{})
 
 func (Ed25519Suite) TestUnmarshalEd25519(c *C) {
@@ -37,6 +39,32 @@ func (Ed25519Suite) TestUnmarshalEd25519(c *C) {
 	c.Assert(verifier.UnmarshalPublicKey(badKey), IsNil)
 }
 
+func FuzzUnmarshal_Ed25519(f *testing.F) {
+	f.Fuzz(func(t *testing.T, s string) {
+		if len(s) <= keySize { // ed25519 requires strings of len greater than 32
+			t.Skip()
+		}
+		c := &C{}
+
+		pub, _, err := ed25519.GenerateKey(strings.NewReader(s))
+		c.Assert(err, IsNil)
+
+		publicKey, err := json.Marshal(map[string]string{
+			"public": hex.EncodeToString(pub),
+		})
+		c.Assert(err, IsNil)
+
+		badKey := &data.PublicKey{
+			Type:       data.KeyTypeEd25519,
+			Scheme:     data.KeySchemeEd25519,
+			Algorithms: data.HashAlgorithms,
+			Value:      publicKey,
+		}
+		verifier := NewEd25519Verifier()
+		c.Assert(verifier.UnmarshalPublicKey(badKey), IsNil)
+	})
+
+}
 func (Ed25519Suite) TestUnmarshalEd25519_Invalid(c *C) {
 	badKeyValue, err := json.Marshal(true)
 	c.Assert(err, IsNil)
@@ -50,6 +78,30 @@ func (Ed25519Suite) TestUnmarshalEd25519_Invalid(c *C) {
 	c.Assert(verifier.UnmarshalPublicKey(badKey), ErrorMatches, "json: cannot unmarshal.*")
 }
 
+func FuzzUnmarshalEd25519_Invalid(f *testing.F) {
+	f.Fuzz(func(t *testing.T, s string) {
+		c := &C{}
+		if len(s) > keySize {
+			// We are only testing for lengths greater than the
+			// keySize because all other data could be successful.
+			t.Skip()
+		}
+		badKeyValue, err := json.Marshal(s)
+
+		if err != nil {
+			t.Skip()
+		}
+		badKey := &data.PublicKey{
+			Type:       data.KeyTypeEd25519,
+			Scheme:     data.KeySchemeEd25519,
+			Algorithms: data.HashAlgorithms,
+			Value:      badKeyValue,
+		}
+		verifier := NewEd25519Verifier()
+		c.Assert(verifier.UnmarshalPublicKey(badKey), ErrorMatches, "json: cannot unmarshal.*")
+	})
+
+}
 func (Ed25519Suite) TestUnmarshalEd25519_FastFuzz(c *C) {
 	verifier := NewEd25519Verifier()
 	for i := 0; i < 50; i++ {
@@ -96,4 +148,18 @@ func (Ed25519Suite) TestSignVerify(c *C) {
 	pubKey, err := GetVerifier(publicData)
 	c.Assert(err, IsNil)
 	c.Assert(pubKey.Verify(msg, sig), IsNil)
+}
+func FuzzSignVerfiy(f *testing.F) {
+	f.Fuzz(func(t *testing.T, s string) {
+		c := &C{}
+		signer, err := GenerateEd25519Key()
+		c.Assert(err, IsNil)
+		msg := []byte(s)
+		sig, err := signer.SignMessage(msg)
+		c.Assert(err, IsNil)
+		publicData := signer.PublicData()
+		pubKey, err := GetVerifier(publicData)
+		c.Assert(err, IsNil)
+		c.Assert(pubKey.Verify(msg, sig), IsNil)
+	})
 }
